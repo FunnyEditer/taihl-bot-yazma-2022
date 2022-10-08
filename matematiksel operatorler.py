832d47991a0c6b7fa40b23b9fa96e829816463fe
FunnyEditer <ahmedesaddemir@gmail.com>
2022-10-08 16:58:17 +0300

    matematiksel operatorlar öğrenildi

---
+++ matematiksel operatorler.py
@@ -0,0 +1,28 @@
+
+"""
+Toplama: +
+Çıkarma: -
+Çarpma: *
+Bölme: /
+Üs: **
+Mod: %
+Tam Bölme: //
+"""
+# TOPLAMA
+print(873+255)
+
+#ÇIKARMA
+print(524-353)
+
+#ÇARPMA
+print(12*25)
+
+#BÖLME
+print(845/65)
+
+#ÜS ALMA
+print(2**3)
+
+#TAM BÖLME
+print(40//3)
+print(-40//3)
