6aad241b4abcaaebde42e77b786dc82c3bd8bc87
FunnyEditer <ahmedesaddemir@gmail.com>
2022-10-19 21:54:43 +0300

    e
    e

---
+++ 3.haftaodevı.py
@@ -0,0 +1,28 @@
+ url = "https://teknolojiaihl.meb.k12.tr"
+
+# 1- "url" içinde kaç karakter olduğunu yazdır.
+print(len(url))
+
+# 2- "url" içindeki "meb" sözcüğünü ekrana yazdırın.
+print(len(24))
+print(len(25))
+print(len(26))
+# 3- "url" içindeki "k12" sözcüğünü ekrana yazdırın.
+print(len(28))
+print(len(29))
+print(len(30))
+# 4- Kullanıcıdan adını, en sevdiği yemeği alın ve cümle içinde yazdırın.
+# ÖRNEK CÜMLE: Adınız: Eren. En sevdiğiniz yemek: Pırasa
+ad=input('isminiz)
+yemek=input('en sevdiğiniz yemek')
+print('isminiz'+ad+'en sevdiğiniz yemek'+yemek)
+# 5- Öğrencinin 2 sınav notunu alın. Notunu hesaplayıp cümle içinde yazdırın.
+# 1. sınav oranı: %35
+# 2. sınav oranı: %65
+# ÖRNEK CÜMLE: 1. sınav: 70 puan, 2. sınav: 90 puan, Notunuz: 83.0
+x=0.64
+y=0.44
+print( (55*x)+(54*y))
+# 6- Kullanıcının adını alın ve yan yana 100 defa yazdırın.
+isim= input(''isminiz: '')
+print(isim*100)
---
+++ git.py
@@ -0,0 +1,4 @@
+x = int(input ("birinci sayıyı girin :"))
+y =int (input ("ikinci sayıyı girin:"))
+
+print(x+y)
