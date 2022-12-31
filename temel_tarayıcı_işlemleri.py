73b2f8a9ce1d1dc3683d56bfa597dca51800874b
FunnyEditer <114801683+FunnyEditer@users.noreply.github.com>
2022-12-31 11:00:06 +0300

    bitti

--- temel_tarayıcı_işlemleri.py
+++ temel_tarayıcı_işlemleri.py
@@ -1,20 +1,14 @@
-#selenium ve diğer paketler içeri aktarılır
-
-
-
-
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
-
+# tarayıcı nesenemizi import edelim
+from moduller.tarayici import Tarayici
 from time import sleep
-tarayici = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+tarayici_nesnesi = Tarayici()
+tarayici =tarayici_nesnesi.al()
 
 #tarayıcıda gezinme
 tarayici.get("https://teknolojiaihl.meb.k12.tr")
 print(tarayici.current_url)
 sleep(1)
-
 tarayici.get("ttps://teknolojiaihl.meb.k12.tr/34/40/766892/teskilat_semasi.html")
 sleep(1)
 
@@ -39,4 +33,11 @@ print(tarayici.get_window_position())
 
 # pencerenin pozisyonunu ayarlayalım
 tarayici.set_window_position(100, 500)
+sleep(2)
+
+tarayici.maximize_window()
+sleep(2)
+
+# pencereyi simge durumuna küçültelim
+tarayici.minimize_window()
 sleep(2)
